33d91f32c4f79c75b13f5e45455dc6b1b3f4e078
Ben Hudson <ben@hudsonsathome.com>
2021-01-28 17:04:22 -0500

    Updated setup.py to include README

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import setup
+from os import path
+
+readme_path = path.join(path.abspath(path.dirname(__file__)), 'README.md')
+with open(readme_path) as readme:
+    long_description = readme.read()
 
 setup(name='lkh',
     description='Super simple Python wrapper for LKH-3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Ben Hudson',
     author_email='benhudson@fastmail.com',
-    version='1.0',
+    version='1.0.1',
     license='DBAD',
     py_modules=['lkh'],
     url='https://github.com/ben-hudson/pylkh',
@@ -12,6 +19,6 @@ setup(name='lkh',
     keywords=['TSP', 'CVRP', 'VRP', 'LKH', 'LKH-3'],
     python_requires='>=3.3',
     install_requires=[
-    	'tsplib95'
+        'tsplib95'
     ],
 )
